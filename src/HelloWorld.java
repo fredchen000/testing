8e6d4af5b958635b1e7338974b293b9e6507bff9
陳 亮宇 <fredchen000@hotmail.com>
2018-02-09 18:39:12 +0800

    version two done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,16 @@
 
+import java.util.Date;
+import java.util.Scanner;
+import java.util.Random;
 public class HelloWorld {
+	public static void main(String[] args) {
+		/**
+		 * @author chinkashiwakin
+		 */
+		System.out.println("HelloWorld");
+		System.out.println("Name: Chen, Liang-Yu");
+		System.out.println("StudentID: 20387832");
+		
+	};
 
 }
